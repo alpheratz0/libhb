0d7515009d4760b5935fb4fbf84f3eedd9e4f7f0
alpheratz0 <alpheratz99@protonmail.com>
2022-12-21 22:23:27 -0300

    add examples/stadium_print.c

---
+++ examples/stadium_print.c
@@ -0,0 +1,21 @@
+/*
+   
+	cc -std=c99 -pedantic -Wall -Wextra -Os -s \
+   			-o stadium_print stadium_print.c -ljq -lhb
+
+*/
+#include <hb/stadium.h>
+
+int
+main(int argc, char **argv)
+{
+	struct hb_stadium *stadium;
+	if (argc <= 1)
+		return -1
+	stadium = hb_stadium_from_file(argv[1]);
+	if (!stadium)
+		return 1;
+	hb_stadium_print(stadium);
+	hb_stadium_free(stadium);
+	return 0;
+}
